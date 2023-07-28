22ac96faae8afc1d2b03ed28f8fa92dc3009dd65
Daniel Di Gangi <135088092+Spyral96@users.noreply.github.com>
2023-07-27 23:46:39 -0400

    Update Converting To Parquet Instead.py

--- Converting To Parquet Instead.py
+++ Converting To Parquet Instead.py
@@ -43,9 +43,9 @@ for chunk in pd.read_csv('C://Users/Dan/Desktop/Python Files/NY Calls/311_Servic
 ####################################################################
 #Turning Parquet File into a Data Frame (from locally saved file)
 #chunk counter(per file)
-import pandas as pd
-import os
-import pyarrow
+
+print('starting parquet to data frame')
+
 file_par= 0
 #blank DF
 ny_calls = pd.DataFrame()
@@ -58,24 +58,69 @@ for file in os.listdir("C:/Users/Dan/Desktop/Python Files/NY Calls/all_parquet_f
         filepath = os.path.join("C:/Users/Dan/Desktop/Python Files/NY Calls/all_parquet_files", file)
         
         #reading curent parquet file
-        df = pd.read_parquet(filepath, engine='pyarrow')
+        df = pd.read_parquet(filepath,columns=['Agency Name','Complaint Type','City','Created Date'])
         
-        #any errors that interfer with dtypes (line 58) with be turned into NaN
-        df[['Unique Key','Incident Zip','X Coordinate (State Plane)','Y Coordinate (State Plane)']] = df[['Unique Key','Incident Zip','X Coordinate (State Plane)','Y Coordinate (State Plane)']].apply(pd.to_numeric, errors='coerce') 
-        
-        #using dtypes to decrease file size per column
-        df =df.astype({'Unique Key':'int32','Incident Zip':'float32','X Coordinate (State Plane)':'float32','Y Coordinate (State Plane)':'float32'})
-        
+
         #add  new data frame to blank dataframe(line 48)
-        ny_calls = pd.concat([ny_calls,df])
+        ny_calls = pd.concat([ny_calls, df], ignore_index=True)
+
         
+        ny_calls['Created Date'] = pd.to_datetime(ny_calls['Created Date']).dt.year
+
         #file chunk counter
         file_par +=1
         print(f'{file_par} files have been process/converted')
 
+
+
 #####################################################################
 
+########   converting (Created Date Column) time in simplfied year      ##############
 
+# Creating new DataFrames with altered values
+new_york_popular_agency = pd.DataFrame()
+new_york_cities_data = pd.DataFrame()
+new_york_complaints_data = pd.DataFrame()
+new_york_years_data = pd.DataFrame()
+
+###################Playing around with the data #############
+popular_agency = ny_calls['Agency Name'].value_counts()
+popular_cities = ny_calls['City'].value_counts()
+popular_complaints = ny_calls['Complaint Type'].value_counts()
+calls_per_year = ny_calls['Created Date'].value_counts()
+##############################################################
+
+# Final DF for Agency Data
+new_york_popular_agency = pd.concat([new_york_popular_agency, popular_agency])
+# Turning index into column
+new_york_popular_agency.reset_index(inplace=True)
+
+# Final DF for City Data
+new_york_cities_data = pd.concat([new_york_cities_data, popular_cities])
+# Turning index into column
+new_york_cities_data.reset_index(inplace=True)
+
+# Final DF for Complaint Data
+new_york_complaints_data = pd.concat([new_york_complaints_data, popular_complaints])
+# Turning index into column
+new_york_complaints_data.reset_index(inplace=True)
+
+# Final DF for Years Data
+new_york_years_data = pd.concat([new_york_years_data, calls_per_year], axis=1)
+# Turning index into column
+new_york_years_data.reset_index(inplace=True)
+
+
+
+print('converting to csv files')
+
+#Converting to CSV file
+ny_calls.to_csv('NY York Calls Graphing Data.csv')
+
+transformed_data =pd.DataFrame()
+
+transformed_data = pd.concat([transformed_data,new_york_popular_agency,new_york_cities_data,new_york_complaints_data,new_york_years_data])
+transformed_data.to_csv('transformed_data.csv',index=False)
 ####Time End
 end_time = time.time()
 
@@ -84,7 +129,7 @@ time_taken = end_time - start_time
 
 print(f'This task took {time_taken} seconds!')
 
-#This task took 311.4292800426483 seconds!
-#Yes! Finally it works! and the file is so small.
+#This task took This task took 5236.458662986755 seconds!
+#WIP oh boy too long
 
 
